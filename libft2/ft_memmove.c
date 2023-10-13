63d9ca368aabc2074d9f5ba2095869debb8feec6
semikci <143425880+semikci@users.noreply.github.com>
2023-10-14 01:23:30 +0300

    Add files via upload

---
+++ libft2/ft_memmove.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sekmekci <sekmekci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 00:01:30 by sekmekci          #+#    #+#             */
+/*   Updated: 2023/10/14 00:01:34 by sekmekci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	if (!dest && !src)
+		return (dest);
+	if (dest < src)
+		return (ft_memcpy(dest, src, n));
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	while (n--)
+	{
+		d[n] = s[n];
+	}
+	return (dest);
+}
